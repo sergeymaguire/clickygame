595c64b4b623840f03f8d09c660f0122e36ef790
sergeymaguire <sergeymaguire@gmail.com>
2019-01-29 14:34:16 -0800

    added instructions to the game

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,16 @@ import "./Header.css";
 
 const Header = props => (
   <div className="header">
+     <div className="instructions">
+    Click an image to start a game! Do not click on the same one, if you do you will lose!
+    </div>
     <div className="title">{props.children}</div>
     <div className="scores">
       Score: {props.score} Highscore: {props.highscore}
     </div>
   </div>
+ 
 );
+
 
 export default Header;
